6efc625f7355581ebd167a02314f9c2dee331e1c
Ehan <ehanahamed@ehan.dev>
2023-02-23 18:24:59 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-http.createServer(
-  function (request, response) {
-    console.log("wow");
-    response.write("wow");
-    response.end();
-  }
-).listen(process.env.PORT || 3000);
