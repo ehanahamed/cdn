9c535876b26d1f94e728ba886e7cae03d9f32a81
Ehan <ehanahamed@ehan.dev>
2023-02-27 16:03:32 -0500

    Update and rename curleveryln.sh to dnldfromlist.sh

--- src/curleveryln.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read -r line; do
-  if [ ${line:0:1} != \# ]; then
-    mkdir -p ${line%/*}
-    curl -o $line "https://raw.ehan.dev/$line"
-  fi
-done < $1
---
+++ src/dnldfromlist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# cdn.ehan.dev
+# 
+# dnldfromlist.sh - Download each file from list of paths
+
+while read -r line; do
+# execute for each line of input file
+# with $line as current line's contents
+  if [ ${line:0:1} != \# ]; then
+  # if line does not begin with hashtag (#)
+    mkdir -p ${line%/*}
+    curl -o $line "https://raw.ehan.dev/$line"
+  fi
+done < $1
